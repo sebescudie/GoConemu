1344b3e6707a9adc0be0a100b24d545d62e72f64
sebescudie <seb.espm@gmail.com>
2019-05-11 12:11:26 +0200

    added double quotes around path so it can handle spaces, closes #1

--- go_conemu/__init__.py
+++ go_conemu/__init__.py
@@ -1,8 +1,8 @@
-from fman import DirectoryPaneCommand
+from fman import DirectoryPaneCommand, show_alert
 import subprocess
 
 class GoConemu(DirectoryPaneCommand):
 	def __call__(self):
 		conemu_path = "C:\\Program Files\\ConEmu\\ConEmu64.exe"
-		current_path = rself.pane.get_path()[7:]
-		subprocess.call(conemu_path + " -Dir " + current_path)
+		current_path = self.pane.get_path()[7:]
+		subprocess.call(conemu_path + " -Dir " + '"' + current_path + '"')
